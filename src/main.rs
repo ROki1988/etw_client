48bb92e73fc0615db96f52b8540233cede351226
ROki1988 <roki.swindler@gmail.com>
2017-06-03 17:25:05 +0900

    use ProcessTrace

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate winapi;
 extern crate widestring;
 
+use std::mem;
+use std::ptr;
 use winapi::um::errhandlingapi;
 use winapi::um::eventtrace;
-use std::mem;
 use widestring::WideCString;
 
 const INVALID_PROCESSTRACE_HANDLE: eventtrace::TRACEHANDLE = -1isize as eventtrace::TRACEHANDLE;
@@ -14,20 +15,24 @@ fn main() {
     unsafe {
         l = mem::zeroed();
         l.LoggerName = ws.into_raw();
-        l.EventTraceLogFile_u = eventtrace::EVENT_TRACE_LOGFILE_u([eventtrace::PROCESS_TRACE_MODE_REAL_TIME | eventtrace::PROCESS_TRACE_MODE_EVENT_RECORD]);
+        l.EventTraceLogFile_u =
+            eventtrace::EVENT_TRACE_LOGFILE_u([eventtrace::PROCESS_TRACE_MODE_REAL_TIME |
+                                               eventtrace::PROCESS_TRACE_MODE_EVENT_RECORD]);
         l.EventTraceLogFile_u2 = eventtrace::EVENT_TRACE_LOGFILE_u2([process_event as u64]);
-        
-        let h = eventtrace::OpenTraceW(&mut l);
+        let mut h = eventtrace::OpenTraceW(&mut l);
         if h == INVALID_PROCESSTRACE_HANDLE {
             println!("ERROR 0x{:x}", errhandlingapi::GetLastError());
-        }
-        else {
-            println!("Success");
-            eventtrace::CloseTrace(h);
+        } else {
+            println!("Success: 0x{:x}", h);
+            let r = eventtrace::ProcessTrace(&mut h, 1, ptr::null_mut(), ptr::null_mut());
+            if r != winapi::shared::winerror::ERROR_SUCCESS {
+                println!("ERROR 0x{:x}", r);
+                eventtrace::CloseTrace(h);            
+            }
         }
     }
 }
 
-extern fn process_event(p_event: eventtrace::PEVENT_RECORD) {
-    
+unsafe extern "system" fn process_event(p_event: eventtrace::PEVENT_RECORD) {
+    println!("{:?}", p_event);
 }
