65ef0a9895cd9f8a22a127b922caf96ff3e178f5
ROki1988 <roki.swindler@gmail.com>
2017-10-05 23:12:24 +0900

    def struct

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::mem;
 use std::thread;
 use std::time::Duration;
 use std::ptr;
+use std::collections::HashMap;
 use winapi::um::errhandlingapi;
 use winapi::um::eventtrace;
 use widestring::WideCString;
@@ -23,7 +24,7 @@ fn main() {
         (*prop).Wnode.Guid = eventtrace::SystemTraceControlGuid;
         (*prop).Wnode.ClientContext = 1;
         (*prop).Wnode.Flags = eventtrace::WNODE_FLAG_TRACED_GUID;
-        (*prop).EnableFlags = eventtrace::EVENT_TRACE_FLAG_DISK_IO;
+        (*prop).EnableFlags = eventtrace::EVENT_TRACE_FLAG_PROCESS;
         (*prop).MaximumFileSize = 1;
         (*prop).LogFileMode = eventtrace::EVENT_TRACE_REAL_TIME_MODE;
         (*prop).LoggerNameOffset = mem::size_of::<eventtrace::EVENT_TRACE_PROPERTIES>() as u32;
@@ -121,30 +122,90 @@ unsafe fn show_event_info(p_event: eventtrace::PEVENT_RECORD,
         return;
     }
 
-    for i in 0..(*p_info).TopLevelPropertyCount - 1 {
-        show_property_info(p_event,
-                           p_info,
-                           (*p_info).EventPropertyInfoArray[i as usize],
-                           None)
-    }
+
+    let a = TdhInfo {
+        property: (0..(*p_info).TopLevelPropertyCount - 1)
+            .into_iter()
+            .map(|i| {
+                get_property_info(p_event,
+                                  p_info,
+                                  (*p_info).EventPropertyInfoArray[i as usize])
+            })
+            .collect(),
+    };
+
+    println!("{:?}", a);
 }
 
-unsafe fn show_property_info(p_event: eventtrace::PEVENT_RECORD,
-                             p_info: eventtrace::PTRACE_EVENT_INFO,
-                             property_info: eventtrace::EVENT_PROPERTY_INFO,
-                             struct_name: Option<&WideCString>) {
+#[derive(Debug)]
+struct TdhInfo {
+    property: HashMap<String, TdhProperty>,
+}
+
+#[derive(Debug)]
+enum TdhProperty {
+    Struct(Box<HashMap<String, TdhProperty>>),
+    Map(Box<HashMap<String, TdhProperty>>),
+    WString(WideCString),
+    Int8(i8),
+    UInt8(u8),
+    Int16(i16),
+    UInt16(u16),
+    Int32(i32),
+    UInt32(u32),
+    Int64(i64),
+    UInt64(u64),
+    Float(f32),
+    Double(f64),
+    Bool(bool),
+    Pointer32(u32),
+    Pointer64(u64),
+}
+
+unsafe fn get_property_map(p_event: eventtrace::PEVENT_RECORD,
+                           p_info: eventtrace::PTRACE_EVENT_INFO,
+                           property_info: eventtrace::EVENT_PROPERTY_INFO)
+                           -> HashMap<String, TdhProperty> {
+
+    if (*p_info).TopLevelPropertyCount <= 0 {
+        return HashMap::new();
+    }
+
+    (0..(*p_info).TopLevelPropertyCount - 1)
+        .into_iter()
+        .map(|i| {
+            get_property_info(p_event,
+                              p_info,
+                              (*p_info).EventPropertyInfoArray[i as usize])
+        })
+        .collect()
+}
+
+unsafe fn get_property_info(p_event: eventtrace::PEVENT_RECORD,
+                            p_info: eventtrace::PTRACE_EVENT_INFO,
+                            property_info: eventtrace::EVENT_PROPERTY_INFO)
+                            -> (String, TdhProperty) {
+    let name = read_wstring(p_info as *const u8, property_info.NameOffset as isize);
+
+    if property_info.Flags == eventtrace::PropertyStruct {
+        let p: *mut eventtrace::EVENT_PROPERTY_INFO = &mut (*p_info).EventPropertyInfoArray[0];
+        let r = get_property_map(p_event, p_info, *(p.offset(property_info.EventPropertyInfo_u1.StructType().StructStartIndex as isize)));
+        (name.to_string_lossy(), TdhProperty::Struct(Box::new(r)))
+    } else {
+        get_property_info_non_struct(p_event, p_info, property_info, None)
+            .expect("!!!!!!!!!!!!")
+    }
+}
+unsafe fn get_property_info_non_struct(p_event: eventtrace::PEVENT_RECORD,
+                                       p_info: eventtrace::PTRACE_EVENT_INFO,
+                                       property_info: eventtrace::EVENT_PROPERTY_INFO,
+                                       struct_name: Option<&WideCString>)
+                                       -> Result<(String, TdhProperty), u32> {
     use winapi::shared::ntdef::{PSHORT, PUSHORT, PLONG, PULONG, PULONGLONG, DOUBLE};
     use winapi::shared::minwindef::{PFLOAT, PBOOL};
 
-    let name = read_wstring(p_info as *const u8, property_info.NameOffset as isize);
-    if property_info.Flags == eventtrace::PropertyStruct {
-        let p: *mut eventtrace::EVENT_PROPERTY_INFO = &mut (*p_info).EventPropertyInfoArray[0];
-        for i in 0..property_info.EventPropertyInfo_u1.StructType().NumOfStructMembers - 1 {
-            show_property_info(p_event, p_info, *(p.offset(i as isize + property_info.EventPropertyInfo_u1.StructType().StructStartIndex as isize)), Some(&name));
-        }
-        return;
-    }
 
+    let name = read_wstring(p_info as *const u8, property_info.NameOffset as isize);
     let mut a = if let Some(pat) = struct_name {
         vec![eventtrace::PROPERTY_DATA_DESCRIPTOR {
                  PropertyName: name.as_ptr() as u64,
@@ -165,12 +226,17 @@ unsafe fn show_property_info(p_event: eventtrace::PEVENT_RECORD,
     };
 
     let mut buff_size = 0;
-    eventtrace::TdhGetPropertySize(p_event,
+
+    let status = eventtrace::TdhGetPropertySize(p_event,
                                    0,
                                    ptr::null_mut(),
                                    a.len() as u32,
                                    a.as_mut_ptr(),
                                    &mut buff_size);
+
+    if status != winerror::ERROR_SUCCESS {
+        return Err(status)
+    }
     let mut buff = vec![0u8; buff_size as usize];
     eventtrace::TdhGetProperty(p_event,
                                0,
@@ -181,86 +247,109 @@ unsafe fn show_property_info(p_event: eventtrace::PEVENT_RECORD,
                                buff.as_mut_ptr());
 
     if property_info.EventPropertyInfo_u1.nonStructType().MapNameOffset == 0 {
-        // TODO: struct name
-        let contain_name = name;
-        print!("{:}({:}): ",
-               contain_name.to_string_lossy(),
-               property_info.EventPropertyInfo_u1.nonStructType().InType);
-
-        match property_info.EventPropertyInfo_u1.nonStructType().InType as u32 {
-            eventtrace::TDH_INTYPE_UNICODESTRING => {
-                println!("{:}",
-                         WideCString::from_ptr_str(buff.as_ptr() as *const u16).to_string_lossy())
-            }
-            eventtrace::TDH_INTYPE_ANSISTRING => (),
-            eventtrace::TDH_INTYPE_INT16 => println!("{:}", *(buff.as_ptr() as PSHORT)),
-            eventtrace::TDH_INTYPE_UINT16 => println!("{:}", *(buff.as_ptr() as PUSHORT)),
-            eventtrace::TDH_INTYPE_INT32 => println!("{:}", *(buff.as_ptr() as PLONG)),
-            eventtrace::TDH_INTYPE_UINT32 => println!("{:}", *(buff.as_ptr() as PULONG)),
-            eventtrace::TDH_INTYPE_UINT64 => println!("{:}", *(buff.as_ptr() as PULONGLONG)),
-            eventtrace::TDH_INTYPE_FLOAT => println!("{:}", *(buff.as_ptr() as PFLOAT)),
-            eventtrace::TDH_INTYPE_DOUBLE => println!("{:}", *(buff.as_ptr() as *const DOUBLE)),
-            eventtrace::TDH_INTYPE_BOOLEAN => println!("{:}", *(buff.as_ptr() as PBOOL)),
-            eventtrace::TDH_INTYPE_BINARY => (),
-            eventtrace::TDH_INTYPE_GUID => (),
-            eventtrace::TDH_INTYPE_POINTER => println!("0x{:x}", buff.as_ptr() as u64),
-            eventtrace::TDH_INTYPE_FILETIME => (),
-            eventtrace::TDH_INTYPE_SYSTEMTIME => (),
-            eventtrace::TDH_INTYPE_SID => (),
-            eventtrace::TDH_INTYPE_HEXINT32 => (),
-            eventtrace::TDH_INTYPE_HEXINT64 => (),                   
-            eventtrace::TDH_INTYPE_COUNTEDSTRING => (),  
-            eventtrace::TDH_INTYPE_COUNTEDANSISTRING => (),
-            eventtrace::TDH_INTYPE_REVERSEDCOUNTEDSTRING => (),
-            eventtrace::TDH_INTYPE_REVERSEDCOUNTEDANSISTRING => (),
-            eventtrace::TDH_INTYPE_NONNULLTERMINATEDSTRING => (),
-            eventtrace::TDH_INTYPE_NONNULLTERMINATEDANSISTRING => (),
-            eventtrace::TDH_INTYPE_UNICODECHAR => (),
-            eventtrace::TDH_INTYPE_ANSICHAR => (),
-            eventtrace::TDH_INTYPE_SIZET => (),
-            eventtrace::TDH_INTYPE_HEXDUMP => (),
-            eventtrace::TDH_INTYPE_WBEMSID => (),
-            _ => (),
+        if property_info.Flags == eventtrace::PropertyStruct {
+            let p: *mut eventtrace::EVENT_PROPERTY_INFO = &mut (*p_info).EventPropertyInfoArray[0];
+            let a: HashMap<String, TdhProperty> = (0..property_info.EventPropertyInfo_u1.StructType().NumOfStructMembers - 1)
+        .into_iter()
+        .map(|i| {
+            get_property_info_non_struct(p_event,
+                              p_info,
+                              *(p.offset(i as isize + property_info.EventPropertyInfo_u1.StructType().StructStartIndex as isize)),
+                              Some(&name))
+        })
+        .filter_map(|x| x.ok())
+        .collect();
+            return Ok((name.to_string_lossy(), TdhProperty::Struct(Box::new(a))));
         }
+
+        let contain_name = struct_name.map_or(name.to_string_lossy(), |wc| {
+            format!("{:}.{:}", wc.to_string_lossy(), name.to_string_lossy())
+        });
+
+        let v = match property_info.EventPropertyInfo_u1.nonStructType().InType as u32 {
+            eventtrace::TDH_INTYPE_UNICODESTRING => {
+                TdhProperty::WString(WideCString::from_ptr_str(buff.as_ptr() as *const u16))
+            }
+            //            eventtrace::TDH_INTYPE_ANSISTRING => (),
+            eventtrace::TDH_INTYPE_INT16 => TdhProperty::Int16(*(buff.as_ptr() as PSHORT)),
+            eventtrace::TDH_INTYPE_UINT16 => TdhProperty::UInt16(*(buff.as_ptr() as PUSHORT)),
+            eventtrace::TDH_INTYPE_INT32 => TdhProperty::Int32(*(buff.as_ptr() as PLONG)),
+            eventtrace::TDH_INTYPE_UINT32 => TdhProperty::UInt32(*(buff.as_ptr() as PULONG)),
+            eventtrace::TDH_INTYPE_UINT64 => TdhProperty::UInt64(*(buff.as_ptr() as PULONGLONG)),
+            eventtrace::TDH_INTYPE_FLOAT => TdhProperty::Float(*(buff.as_ptr() as PFLOAT)),
+            eventtrace::TDH_INTYPE_DOUBLE => TdhProperty::Double(*(buff.as_ptr() as *const DOUBLE)),
+            eventtrace::TDH_INTYPE_BOOLEAN => TdhProperty::Bool(*(buff.as_ptr() as PBOOL) != 0),
+            //            eventtrace::TDH_INTYPE_BINARY => (),
+            //            eventtrace::TDH_INTYPE_GUID => (),
+            eventtrace::TDH_INTYPE_POINTER => TdhProperty::Pointer64(buff.as_ptr() as u64),
+            //            eventtrace::TDH_INTYPE_FILETIME => (),
+            //            eventtrace::TDH_INTYPE_SYSTEMTIME => (),
+            //            eventtrace::TDH_INTYPE_SID => (),
+            //            eventtrace::TDH_INTYPE_HEXINT32 => (),
+            //            eventtrace::TDH_INTYPE_HEXINT64 => (),
+            //            eventtrace::TDH_INTYPE_COUNTEDSTRING => (),
+            //            eventtrace::TDH_INTYPE_COUNTEDANSISTRING => (),
+            //            eventtrace::TDH_INTYPE_REVERSEDCOUNTEDSTRING => (),
+            //            eventtrace::TDH_INTYPE_REVERSEDCOUNTEDANSISTRING => (),
+            //            eventtrace::TDH_INTYPE_NONNULLTERMINATEDSTRING => (),
+            //            eventtrace::TDH_INTYPE_NONNULLTERMINATEDANSISTRING => (),
+            //            eventtrace::TDH_INTYPE_UNICODECHAR => (),
+            //            eventtrace::TDH_INTYPE_ANSICHAR => (),
+            //            eventtrace::TDH_INTYPE_SIZET => (),
+            //            eventtrace::TDH_INTYPE_HEXDUMP => (),
+            //            eventtrace::TDH_INTYPE_WBEMSID => (),
+            _ => TdhProperty::UInt16(0),
+        };
+        Ok((contain_name, v))
     } else {
         let map_name = (p_info as *const u8)
             .offset(property_info.EventPropertyInfo_u1.nonStructType().MapNameOffset as isize);
         let map_value = *(buff.as_ptr() as PULONG);
 
         let mut map_buff_size = 0;
-        if eventtrace::TdhGetEventMapInformation(p_event,
-                                                 map_name as *mut u16,
-                                                 ptr::null_mut(),
-                                                 &mut map_buff_size) !=
-           winerror::ERROR_INSUFFICIENT_BUFFER {
-            return;
+        let status = eventtrace::TdhGetEventMapInformation(p_event,
+                                                           map_name as *mut u16,
+                                                           ptr::null_mut(),
+                                                           &mut map_buff_size);
+        if status != winerror::ERROR_INSUFFICIENT_BUFFER {
+            return Err(status);
         }
 
         let map_buff = vec![0u8; map_buff_size as usize];
         let map = map_buff.as_ptr() as eventtrace::PEVENT_MAP_INFO;
 
-        if eventtrace::TdhGetEventMapInformation(p_event,
-                                                 map_name as *mut u16,
-                                                 map,
-                                                 &mut map_buff_size) !=
-           winerror::ERROR_SUCCESS {
-            return;
+        let status = eventtrace::TdhGetEventMapInformation(p_event,
+                                                           map_name as *mut u16,
+                                                           map,
+                                                           &mut map_buff_size);
+        if status != winerror::ERROR_SUCCESS {
+            return Err(status);
         }
 
         if (*map).Flag == eventtrace::EVENTMAP_INFO_FLAG_MANIFEST_VALUEMAP {
-            for i in 0..((*map).EntryCount - 1) {
-                if (*map).MapEntryArray[i as usize].EventMapEntry_u.Value() == &map_value {
+            let a: HashMap<String, TdhProperty> = (0..(*map).EntryCount - 1)
+                .into_iter()
+                .filter(|&x| (*map).MapEntryArray[x as usize].EventMapEntry_u.Value() == &map_value)
+                .map(|i| {
                     let key_name =
                         read_wstring(map as *const u8,
                                      (*map).MapEntryArray[i as usize].OutputOffset as isize);
-                    println!("  {:}:{:}.", key_name.to_string_lossy(), map_value);
-                }
-            }
+                    (key_name.to_string_lossy(), TdhProperty::UInt32(map_value))
+                })
+                .collect();
+
+            Ok((read_wstring((p_info as *const u8),
+                             property_info.EventPropertyInfo_u1
+                                 .nonStructType()
+                                 .MapNameOffset as isize)
+                .to_string_lossy(),
+                TdhProperty::Map(Box::new(a))))
+        } else {
+            Err(1)
         }
-
-
-    };
+    }
 }
+
 
 unsafe fn read_wstring(p: *const u8, offset: isize) -> WideCString {
     WideCString::from_ptr_str(p.wrapping_offset(offset) as *const u16)
